cf13a6dd3322549bbcc2698aa32945cac87c5d7b
Matthew Todor <unclejunglegeorge@gmail.com>
2023-03-31 16:45:22 -0600

    adding in route for deployment to build dir

--- server/server.js
+++ server/server.js
@@ -19,6 +19,13 @@ app.use(
 );
 // turn on routes
 app.use(routes);
+
+if (process.env.NODE_ENV == "production") {
+	app.use(express.static(path.join(__dirname, "../client/build")));
+	app.get("/", (req, res) => {
+		res.sendFile(path.join(__dirname, "../client/build/index.html"));
+	});
+}
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
 	app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
