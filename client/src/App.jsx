3f6fbdeff645b8d2bf49c5b790c3f38e74dce2fc
Matthew Todor <unclejunglegeorge@gmail.com>
2023-03-31 16:06:08 -0600

    it's not updating?

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar/index.js";
+import Navbar from "components/Navbar/index.js";
 
 const App = () => {
 	return (
