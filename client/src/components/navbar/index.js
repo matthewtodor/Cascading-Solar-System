dcd03895c79483976c1d5d83c2af0d11bfb35a41
BluSkreen <jasonduran303@gmail.com>
2023-03-30 22:56:02 -0600

    fixed merge conflicts

--- client/src/components/navbar/index.js
+++ client/src/components/navbar/index.js
@@ -2,13 +2,7 @@ import "./style.css";
 import { Link, useLocation } from "react-router-dom";
 
 function Navbar() {
-	//const history = useHistory();
-	//const goBack = () => {
-	//    history.goBack()
-	//}
-
     let location = useLocation();
-
 	return (
 		<div className="nav">
 			<h1><Link to={`../..`} relative="path">Cascading Solar System</Link></h1>
@@ -42,40 +36,7 @@ function Navbar() {
                     Neptune
                 </Link>
             </div>
-            { /* location.pathname === "/"
-                ?(<div className="dropdown">
-                    <span className="dropdown-head">Planets</span>
-                    <Link to={`planets/venus`} className="nav-link venus-link">
-                        Venus
-                    </Link>
-                    <Link to={`planets/mercury`} className="nav-link mercury-link">
-                        Mercury
-                    </Link>
-                    <Link to={`planets/earth`} className="nav-link earth-link">
-                        Earth
-                    </Link>
-                    <Link to={`planets/mars`} className="nav-link mars-link">
-                        Mars
-                    </Link>
-                    <Link to={`planets/jupiter`} className="nav-link jupiter-link">
-                        Jupiter
-                    </Link>
-                    <Link to={`planets/saturn`} className="nav-link saturn-link">
-                        Saturn
-                    </Link>
-                    <Link to={`planets/uranus`} className="nav-link uranus-link">
-                        Uranus
-                    </Link>
-                    <Link to={`planets/neptune`} className="nav-link neptune-link">
-                        Neptune
-                    </Link>
-                </div>)
-                :(<div className="back">
-                    <Link to={`../..`} relative="path">Back</Link>
-                </div>)
-            */}
 		</div>
 	);
 }
-
 export default Navbar;
