02c682a3921b92784fb26e2027167a7d76966f1b
Matthew Todor <unclejunglegeorge@gmail.com>
2023-03-31 17:55:06 -0600

    testing FURTHER

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,6 +4,7 @@ require("dotenv").config();
 let sequelize;
 
 if (process.env.JAWSDB_URL) {
+	console.log(process.env.JAWSDB_URL);
 	sequelize = new Sequelize(process.env.JAWSDB_URL);
 } else {
 	sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
