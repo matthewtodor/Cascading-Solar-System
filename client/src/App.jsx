0df124a92423eecb2e3a1621aee2895696595d02
Matthew Todor <unclejunglegeorge@gmail.com>
2023-03-31 16:03:46 -0600

    still trying to resolve the same error

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from "react-router-dom";
 import "./App.css";
-import Navbar from "./components/Navbar/index";
+import Navbar from "./components/Navbar/index.js";
 
 const App = () => {
 	return (
