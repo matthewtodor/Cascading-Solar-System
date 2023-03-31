5a18a8073fde1c44060bd481d7cbe9b60f62693c
Matthew Todor <unclejunglegeorge@gmail.com>
2023-03-31 17:34:22 -0600

    checking to see if information on window.location object is correct on heroku

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -5,6 +5,7 @@ import PlanetPage from "../pages/Planet Page";
 import SolarSystem from "../components/SolarSystem";
 const apiUrl = process.env.REACT_APP_API_URL || `${window.location.protocol}//${window.location.hostname}:${window.location.port}/api/`;
 console.log(process.env.REACT_APP_API_URL);
+console.log(`${window.location.protocol}//${window.location.hostname}:${window.location.port}/api/`);
 const router = createBrowserRouter([
 	{
 		path: "/",
