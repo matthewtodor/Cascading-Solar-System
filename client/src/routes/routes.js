32573e1b504930f269c881f08e55f63d7005beba
Matthew Todor <unclejunglegeorge@gmail.com>
2023-04-01 14:33:32 -0600

    changes to folder name

--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -3,6 +3,8 @@ import App from "../App";
 import ErrorPage from "../pages/Error Page";
 import PlanetPage from "../pages/Planet Page";
 import SolarSystem from "../components/SolarSystem";
+const apiUrl = process.env.REACT_APP_API_URL || "/api/";
+
 const router = createBrowserRouter([
 	{
 		path: "/",
@@ -19,7 +21,7 @@ const router = createBrowserRouter([
 				loader: async ({ params }) => {
 					let results;
 					try {
-						const res = await fetch(`http://localhost:3001/api/${params.name}`, {
+						const res = await fetch(`${apiUrl}${params.name}`, {
 							method: "GET",
 							headers: {
 								"Content-Type": "application/json",
