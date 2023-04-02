fe1e408688faf1adbc0f28b14a6558d58ee52dec
Matthew Todor <unclejunglegeorge@gmail.com>
2023-04-02 17:35:27 -0600

    added error page styling

--- client/src/pages/Error Page/index.jsx
+++ client/src/pages/Error Page/index.jsx
@@ -1,16 +1,20 @@
-import { useRouteError } from "react-router-dom";
-
+import { useRouteError, Link } from "react-router-dom";
+import "../../App.css";
+import "./style.css";
 export default function ErrorPage() {
-  const error = useRouteError();
-  console.error(error);
+	const error = useRouteError();
+	console.error(error);
 
-  return (
-    <div id="error-page">
-      <h1>Oops!</h1>
-      <p>Sorry, an unexpected error has occurred.</p>
-      <p>
-        <i>{error.statusText || error.message}</i>
-      </p>
-    </div>
-  );
+	return (
+		<main className="App">
+			<article className="error-page">
+				<h1>404</h1>
+				<h2>Oops!</h2>
+				<p>You're lost in space!</p>
+				<h3>
+					<Link to="/">Go Home</Link>
+				</h3>
+			</article>
+		</main>
+	);
 }
--- client/src/routes/routes.js
+++ client/src/routes/routes.js
@@ -2,7 +2,6 @@ import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
 import ErrorPage from "../pages/Error Page";
 import PlanetPage from "../pages/Planet Page";
-import SolarSystem from "../components/SolarSystem";
 import Home from "../pages/Home";
 const apiUrl = process.env.REACT_APP_API_URL || "/api/";
 const router = createBrowserRouter([
@@ -12,33 +11,42 @@ const router = createBrowserRouter([
 		errorElement: <ErrorPage />,
 		children: [
 			{
-				path: "/",
-				element: <Home scale="100" />,
-			},
-			{
-				path: "planets/:name",
-				element: <PlanetPage />,
-				loader: async ({ params }) => {
-					let results;
-					try {
-						const res = await fetch(`${apiUrl}${params.name}`, {
-							method: "GET",
-							headers: {
-								"Content-Type": "application/json",
-							},
-						});
-						if (res.status === 404) {
-							throw new Response("Not Found", { status: 404 });
-						} else if (res.status === 500) {
-							throw new Response("Server Error", { status: 500 });
-						}
-						results = res.json();
-					} catch (error) {
-						console.log(error);
-					}
-					return results || null;
-				},
-				id: "planets",
+				errorElement: <ErrorPage />,
+				children: [
+					{
+						path: "/",
+						element: <Home scale="100" />,
+					},
+					{
+						path: "planets",
+						element: <ErrorPage />,
+					},
+					{
+						path: "planets/:name",
+						element: <PlanetPage />,
+						loader: async ({ params }) => {
+							let results;
+							try {
+								const res = await fetch(`${apiUrl}${params.name}`, {
+									method: "GET",
+									headers: {
+										"Content-Type": "application/json",
+									},
+								});
+								if (res.status === 404) {
+									throw new Response("Not Found", { status: 404 });
+								} else if (res.status === 500) {
+									throw new Response("Server Error", { status: 500 });
+								}
+								results = res.json();
+							} catch (error) {
+								console.log(error);
+							}
+							return results || null;
+						},
+						id: "planets",
+					},
+				],
 			},
 		],
 	},
