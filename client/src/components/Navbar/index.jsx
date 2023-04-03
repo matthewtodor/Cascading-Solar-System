d54fb658e74eccb65b8d08a5f01944da610490c2
Matthew Todor <unclejunglegeorge@gmail.com>
2023-04-02 21:44:16 -0600

    modified navbar for mobile

--- client/src/components/Navbar/index.jsx
+++ client/src/components/Navbar/index.jsx
@@ -4,10 +4,6 @@ import { Link, useLocation } from "react-router-dom";
 function Navbar() {
 	const planets = ["mercury", "venus", "earth", "mars", "jupiter", "saturn", "uranus", "neptune"];
 
-	var topValue = 20;
-	const setTop = () => {
-		topValue = topValue + 35;
-	};
 	let location = useLocation();
 	return (
 		<nav className="nav">
@@ -15,25 +11,26 @@ function Navbar() {
 				<div className="dropdown-head">
 					<p>Explore</p>
 				</div>
-				{planets.map((planet) => {
-					setTop();
-					if (location.pathname.slice(9) !== planet) {
-						return (
-							<Link key={planet} to={`/planets/${planet}`} className={`nav-link`} style={{ top: topValue }}>
-								{`${planet.slice(0, 1).toUpperCase() + planet.slice(1)}`}
-							</Link>
-						);
-					} else {
-						return (
-							<div key={planet} className={`nav-link`} style={{ top: topValue }} id="disabled-link">
-								{`${planet.slice(0, 1).toUpperCase() + planet.slice(1)}`}
-							</div>
-						);
-					}
-				})}
-				<Link to={`/`} className={`nav-link`} style={{ top: 335 }}>
-					Home
-				</Link>
+				<div className="dropdown-links">
+					{planets.map((planet) => {
+						if (location.pathname.slice(9) !== planet) {
+							return (
+								<Link key={planet} to={`/planets/${planet}`} className={`nav-link`}>
+									{`${planet.slice(0, 1).toUpperCase() + planet.slice(1)}`}
+								</Link>
+							);
+						} else {
+							return (
+								<div key={planet} className={`nav-link`} id="disabled-link">
+									{`${planet.slice(0, 1).toUpperCase() + planet.slice(1)}`}
+								</div>
+							);
+						}
+					})}
+					<Link to={`/`} className={`nav-link`}>
+						Home
+					</Link>
+				</div>
 			</div>
 		</nav>
 	);
