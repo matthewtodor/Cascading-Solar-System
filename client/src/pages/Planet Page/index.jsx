e130fd6289e571ada115290a7526f660157cccfd
Eli Wood <mreliwood1@gmail.com>
2023-04-03 12:33:35 -0700

    reinstall react router dom

--- client/src/pages/Planet Page/index.jsx
+++ client/src/pages/Planet Page/index.jsx
@@ -1,12 +1,12 @@
-import React from "react";
-import Details from "../../components/Details";
-import Planet from "../../components/Planet";
-import { useRouteLoaderData } from "react-router-dom";
-import "./style.css";
+import React from 'react';
+import Details from '../../components/Details';
+import Planet from '../../components/Planet';
+import { useRouteLoaderData } from 'react-router-dom';
+import './style.css';
 const PlanetPage = () => {
-	const data = useRouteLoaderData("planets");
+	const data = useRouteLoaderData('planets');
 	let name = data.name;
-	const mediaQuerySmall = window.matchMedia("(max-width: 768px)");
+	const mediaQuerySmall = window.matchMedia('(max-width: 768px)');
 	const handleChange = (e) => {
 		if (e.matches) {
 			return true;
@@ -16,15 +16,15 @@ const PlanetPage = () => {
 	handleChange(mediaQuerySmall);
 
 	return (
-		<main className="planet-page">
-			<figure className="planet-page-planet-container">
-				<Planet name={name} scale={handleChange(mediaQuerySmall) ? "170" : "100"} />
+		<main className='planet-page'>
+			<figure className='planet-page-planet-container'>
+				<Planet name={name} scale={handleChange(mediaQuerySmall) ? '170' : '100'} />
 			</figure>
-			<article className="planet-page-details-container">
-				<div className="planet-page-name">
+			<article className='planet-page-details-container'>
+				<div className='planet-page-name'>
 					<h1>{name}</h1>
 				</div>
-				<div className="planet-page-details-content">
+				<div className='planet-page-details-content'>
 					<Details name={data.name} size={data.size} facts={data.facts} moons={data.moons} />
 				</div>
 			</article>
