ce0dd74016b13b60b4053ce336a72b211b9b0128
Matthew Todor <unclejunglegeorge@gmail.com>
2023-04-06 12:30:31 -0600

    fixed up mobile view on home page

--- client/src/components/HomePanel/index.jsx
+++ client/src/components/HomePanel/index.jsx
@@ -10,6 +10,7 @@ const HomePanel = () => {
 					<div className="scroll-container">
 						<section className="facts-container" id="solar-system-facts-container">
 							<h2>Welcome to our galactic home!</h2>
+							<p>To find out information about the planets, click on them, or use the navigation panel!</p>
 							<p>The solar system we live in is massive, despite it currently fitting on this screen! It contains:</p>
 							<ul>
 								<li>8 planets</li>
@@ -17,7 +18,6 @@ const HomePanel = () => {
 								<li>1 asteroid belt (between Mars and Saturn)</li>
 							</ul>
 							<p>It is within the Milky Way galaxy, and is the only known location of life</p>
-							<p>To find out information about the planets, click on them, or use the navigation panel!</p>
 						</section>
 					</div>
 				</section>
