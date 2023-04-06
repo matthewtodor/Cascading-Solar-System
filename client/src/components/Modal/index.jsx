7435b657456aaa457d0eed2662d2307061ecb2f6
Matthew Todor <unclejunglegeorge@gmail.com>
2023-04-05 23:14:30 -0600

    added functionality for modal, only populates in up to 75vh, then triggers scroll bar

--- client/src/components/Modal/index.jsx
+++ client/src/components/Modal/index.jsx
@@ -4,26 +4,27 @@ export default function MoonModal({ show, set, moon }) {
 		<>
 			{show ? (
 				<div className="modal" onClick={() => set()}>
-					<div className="modal-content" onClick={(e) => e.stopPropagation()}>
-						<div className="scroll-container">
-							<header>
-								<h2>{moon[0]}</h2>
-							</header>
-							<aside>
-								<h3>Size</h3>
-								<p>
-									<strong>Radius:</strong> {moon[2]} miles
-								</p>
-								<p>
-									<strong>Diameter:</strong> {moon[3]} miles
-								</p>
-								<p>
-									<strong>Circumference:</strong> {moon[4]} miles
-								</p>
-							</aside>
-							<article>
-								<span>{moon[1]}</span>
-							</article>
+					<div className="modal-content-outer" onClick={(e) => e.stopPropagation()}>
+						<div className="modal-content-inner">
+							<div className="scroll-container">
+								<header>
+									<h2>{moon[0]}</h2>
+								</header>
+								<aside>
+									<p>
+										<strong>Radius:</strong> {moon[2]} miles
+									</p>
+									<p>
+										<strong>Diameter:</strong> {moon[3]} miles
+									</p>
+									<p>
+										<strong>Circumference:</strong> {moon[4]} miles
+									</p>
+								</aside>
+								<article>
+									<span>{moon[1]}</span>
+								</article>
+							</div>
 						</div>
 					</div>
 				</div>
