e168ddbeabaad858a1eb4c95199b665936373126
Matthew Todor <unclejunglegeorge@gmail.com>
2023-04-10 22:00:28 -0600

    working on home page

--- client/src/components/HomePanel/index.jsx
+++ client/src/components/HomePanel/index.jsx
@@ -8,9 +8,11 @@ const HomePanel = () => {
 			<section className="home-panel-outer">
 				<section className="home-panel-inner">
 					<div className="scroll-container">
-						<section className="facts-container" id="solar-system-facts-container">
+						<section>
 							<h2>Welcome to our galactic home!</h2>
 							<p>To find out information about the planets, click on them, or use the navigation panel!</p>
+						</section>
+						<section className="facts-container" id="solar-system-facts-container">
 							<p>The solar system we live in is massive, despite it currently fitting on this screen! It contains:</p>
 							<ul>
 								<li>8 planets</li>
@@ -18,6 +20,14 @@ const HomePanel = () => {
 								<li>1 asteroid belt (between Mars and Saturn)</li>
 							</ul>
 							<p>It is within the Milky Way galaxy, and is the only known location of life</p>
+						</section>
+						<section id="solar-system-github">
+							<p>
+								If you are interested in how this website was build, please check out our{" "}
+								<a href="https://github.com/Ccatalyst/Cascading-Solar-System" target="_blank" rel="noreferrer">
+									Github!
+								</a>
+							</p>
 						</section>
 					</div>
 				</section>
